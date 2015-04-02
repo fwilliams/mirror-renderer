92656dcc81c38c48e83ea0ff78a768a10c79475e
Francis Williams <francis@fwilliams.org>
2015-04-02 18:53:59 -0400

    stub for camera class

---
+++ Camera.cpp
@@ -0,0 +1,18 @@
+/*
+ * camera.cpp
+ *
+ *  Created on: Apr 2, 2015
+ *      Author: francis
+ */
+
+#include "Camera.h"
+
+Camera::Camera() {
+  // TODO Auto-generated constructor stub
+
+}
+
+Camera::~Camera() {
+  // TODO Auto-generated destructor stub
+}
+
---
+++ Camera.h
@@ -0,0 +1,12 @@
+#include <glm/glm.hpp>
+
+#ifndef CAMERA_H_
+#define CAMERA_H_
+
+class Camera {
+public:
+  Camera();
+  virtual ~Camera();
+};
+
+#endif /* CAMERA_H_ */
